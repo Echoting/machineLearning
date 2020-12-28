1055f888276ae01f0895a0cc526d1aab12ae790c
Echoting <870917335@qq.com>
2020-12-28 17:38:14 +0800

    chore: project init & 线性回归可视化训练数据

---
+++ linear-regression/tensorflow.js
@@ -0,0 +1,13 @@
+import * as tfvis from '@tensorflow/tfjs-vis'
+
+window.onload = function () {
+
+    const xs = [1, 2, 3, 4]
+    const ys = [1, 3, 5, 7]
+
+    tfvis.render.scatterplot(
+        {name: '线性回归训练集'},
+        {values: xs.map((x, index) => ({x, y: ys[index]}))},
+        {xAxisDomain: [0, 5], yAxisDomain: [0, 8]}
+    )
+}
